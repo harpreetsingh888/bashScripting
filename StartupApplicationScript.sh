35b4bb96f4332ee37ff29206b56d26c7cdfd4c5a
HarpreetSingh <harpreet.singh@websitetravel.com>
2015-12-03 14:47:41 +1300

    Create StartupApplicationScript.sh

---
+++ StartupApplicationScript.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#@Description: This script runs work related application when i start my laptop only on weekdays
+#@Info: Before you run this script do the following:
+# 1) Create a file to log error by using the following commands
+#    cd /home/$userName/                     $userName denotes your local directory name setup
+#    touch startupApplicationlog.txt         This command will create the text file
+# 2) As you can see I am running yakuake(terminal), google chrome, scudcloud(slack), skype, thunderbird and phpstorm
+#    but you might need to run some different applications. To do so, make sure the executable file is in /usr/bin/ or
+#    you have created a soft link for the executable in this directory. For example - Script file to run phpstorm was
+#    /usr/local/PHPStorm-141/bin/phpstorm.sh, so i create a sym link using following command:
+#    ln -s /usr/local/PHPStorm-141/bin/phpstorm.sh /usr/bin/phpstormlink
+
+DATE=$(date)
+DAY=$(date +%a)
+CURRENTHOUR=$(date +%k%M)
+CURRENTMINUTE=$(date +%k%M)
+LOGFILE=/home/harpreet/startupApplicationlog.txt
+
+if [ $DAY -ne 'Sat' ] || [ $DAY -ne 'Sun' ]
+then
+    if [ $CURRENTHOUR -eq 9 ]
+    then
+        #run all the application related to work
+        yakuake || echo "Not able to load yakuake when the system started on $DATE\n"
+        sleep 2
+        google-chrome || echo "Not able to load google chrome when the system started on $DATE\n"
+        sleep 2
+        scudcloud || echo "Not able to load scudcloud when the system started on $DATE\n"
+        sleep 2
+        skype || echo "Not able to load skype when the system started on $DATE\n"
+        sleep 2
+        thunderbird || echo "Not able to load thunderbird when the system started on $DATE\n"
+        sleep 2
+        phpstormlink || echo "Not able to load phpstorm when the system started on $DATE\n"
+        sleep 2
+    fi
+fi
