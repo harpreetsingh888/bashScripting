71d259b8cfe529e750946dcb794e5cf3444e3970
HarpreetSingh <harpreet.singh@websitetravel.com>
2015-12-04 14:57:35 +1300

    Initial commit

---
+++ ExtendedStartupApplication.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#@Description: This script runs work related application when i start my laptop only on weekdays
+#@Info: Before you run this script do the following:
+# 1) Create a file to log error by using the following commands
+#    cd /home/$userName/                     $userName denotes your local directory name setup
+#    touch startupApplicationlog.txt         This command will create the text file
+# 2) As you can see I am running yakuake(terminal), google chrome, scudcloud(slack), skype, thunderbird and phpstorm
+#    but you might need to run some different applications. To do so, make sure the executable file is in /usr/bin/ or
+#    you have created a soft link for the executable in this directory. For example - Script file to run phpstorm was
+#    /usr/local/PHPStorm-141/bin/phpstorm.sh, so i create a sym link using following command:
+#    ln -s /usr/local/PHPStorm-141/bin/phpstorm.sh /usr/bin/phpstormlink
+# 3) Add this file to /usr/bin/ or usr/local/bin/, I went for /usr/bin/ and make it executable using
+#    chmod +x startupApplication.sh
+# 4) In Ubuntu, search for "Startup Application" application and click on "Add" button.
+# 5) Write the name than click on browse and search for /usr/bin/startupApplication.sh or /usr/local/bin/startupApplication.sh
+#    where ever you have saved your script file and hit "Add"
+
+DATE=$(date)
+DAY=$(date +%a)
+CURRENTHOUR=$(date +%k)
+CURRENTMINUTE=$(date +%M)
+LOGFILE=/home/harpreet/startupApplicationlog.txt
+#declare -a applications=('yakuake' 'google-chrome' 'scudcloud' 'skype' 'thunderbird' 'phpstormlink')
+declare -a CHECKAPPLICATIONS=("thunderbird" "skype")
+declare -a RUNAPPLICATIONS=()
+APPLICATIONS=''
+
+if [ $DAY!=Sat ] && [ $DAY!=Sun ];
+then
+    if [ $CURRENTHOUR -gt 0 ]
+    then
+        for i in ${!CHECKAPPLICATIONS[@]}; do
+            OUTPUT=$(pgrep -n '$i')
+
+            if [ -z "$OUTPUT" ];
+            then
+                CURRENTAPP="${CHECKAPPLICATIONS[$i]}"
+                RUNAPPLICATIONS+=($CURRENTAPP)
+            fi
+        done
+
+        pos=$(( ${#RUNAPPLICATIONS[*]} - 1 ))
+        last=${RUNAPPLICATIONS[$pos]}
+
+        for j in ${RUNAPPLICATIONS[@]}; do
+              if [[ $j == $last ]]
+              then
+                 APPLICATIONS="$APPLICATIONS$j"
+              else
+                 APPLICATIONS="$APPLICATIONS$j & "
+              fi
+        done
+        $APPLICATIONS || echo "Not able to load applications when the system started on $DATE\n" > $LOGFILE
+    fi
+else
+exit
+fi
